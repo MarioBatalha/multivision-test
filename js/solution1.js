e7e9dfaa049fdafaca8ede88f89c309a6def08a3
MarioBatalha <mariobatalha9@gmail.com>
2023-01-20 16:55:20 +0100

    feat: add first solution

---
+++ js/solution1.js
@@ -0,0 +1,28 @@
+import fetch from "node-fetch";
+
+export const letter = {
+  get handleUsersWithPosts() {
+    return (async function getUserInfoAndPostInfo() {
+      try {
+        const users = await fetch("https://jsonplaceholder.typicode.com/users")
+          .then((res) => res.json())
+          .then((data) => data);
+    
+        const posts = await fetch("https://jsonplaceholder.typicode.com/posts")
+          .then((res) => res.json())
+          .then((data) => data);
+    
+          const usersWithPosts = users.map((user) => ({
+          ...user,
+          posts: posts.filter(({ userId }) => userId === user.id),
+        }));
+    
+        return usersWithPosts
+      } catch (error) {
+        console.log(error, "Erro ao carregar dados ");
+      }
+    })();
+  },
+};
+
+console.log(await letter.handleUsersWithPosts)
